07a0f35fa4819834b5896679725c834095252440
MK Rahal <mkrahal@users.noreply.github.com>
2019-06-03 02:10:45 +0000

    sanitizing

--- shopify_scaffolding/urls.py
+++ shopify_scaffolding/urls.py
@@ -2,9 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from installer.views import login, authenticate, finalize, logout, webhook, activateRecurringCharge
-from dashboard.views import index, initial_setup, customize_popups, advanced_settings, mailchimpauth, faq, dp_activities, dp_edit, dp_create
+from dashboard.views import index,
 from scripttags.views import scripttagsrc1
-from gdpr.views import ipcheck, gdpr_request
 
 urlpatterns = [
                 #url(r'admin/', admin.site.urls),
@@ -19,23 +18,8 @@ urlpatterns = [
 
                 # Dashboard
                 url(r'^$', index, name='root_path'),
-                url(r'^home/$', index, name='root_path2'),
-                url(r'^settings/$', initial_setup, name='root_settings'),
-                url(r'^custpopup/$', customize_popups, name='root_customize'),
-                url(r'^faq/$', faq, name='faq'),
-                url(r'^advancedsettings/$', advanced_settings, name='root_advanced'),
-                url(r'^dpactivities/$', dp_activities, name='root_activities'),
-                url(r'^dpedit/$', dp_edit, name='root_edit'),
-                url(r'^dpcreate/$', dp_create, name='root_create'),
 
                 # Script Tags
                 url(r'^scriptsrc1/$', scripttagsrc1, name='first_script_tag'),
-                url(r'^ipcheck/$', ipcheck, name='ipcheck'),
-
-                # Data Processing 
-                url(r'^dataevent/$', gdpr_request, name='dataevent'),
-
-                # ESP Integration Endpoints 
-                url(r'^mailchimpauth/$', mailchimpauth, name='mailchimp_oAuth'),
                 
               ]
